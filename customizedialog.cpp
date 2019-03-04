1ee6046f6afc950db2e530423007eb02948c965b
CSReviser <CSReviser@gmail.com>
2019-03-04 20:56:48 +0900

    カスタマイズウインドウの2018年度の一部講座の対応修正

--- customizedialog.cpp
+++ customizedialog.cpp
@@ -36,30 +36,30 @@ typedef struct LineEdit {
 } LineEdit;
 
 QStringList CustomizeDialog::courses = QStringList()
-		<< QString::fromUtf8( "基礎英語1" ) << QString::fromUtf8( "基礎英語2" )
-		<< QString::fromUtf8( "基礎英語3" ) << QString::fromUtf8( "英会話タイムトライアル" )
-		<< QString::fromUtf8( "ラジオ英会話" ) << QString::fromUtf8( "入門ビジネス英語" )
-		<< QString::fromUtf8( "実践ビジネス英語" ) << QString::fromUtf8( "まいにちロシア語" )
-		<< QString::fromUtf8( "英語で読む村上春樹" ) << QString::fromUtf8( "エンジョイ・シンプル・イングリッシュ" )
+		<< QString::fromUtf8( "基礎英語0" ) << QString::fromUtf8( "基礎英語1" ) 
+		<< QString::fromUtf8( "基礎英語2" ) << QString::fromUtf8( "基礎英語3" )
+		<< QString::fromUtf8( "英会話タイムトライアル" ) << QString::fromUtf8( "ラジオ英会話" )
+		<< QString::fromUtf8( "入門ビジネス英語" ) << QString::fromUtf8( "実践ビジネス英語" )
+		 << QString::fromUtf8( "遠山顕の英会話楽習" ) << QString::fromUtf8( "高校生からはじめる「現代英語」" ) 
 		<< QString::fromUtf8( "まいにち中国語" ) << QString::fromUtf8( "まいにちフランス語" )
 		<< QString::fromUtf8( "まいにちイタリア語" ) << QString::fromUtf8( "まいにちハングル講座" )
 		<< QString::fromUtf8( "まいにちドイツ語" ) << QString::fromUtf8( "まいにちスペイン語" )
 		<< QString::fromUtf8( "レベルアップ中国語" ) << QString::fromUtf8( "レベルアップハングル講座" )
-		<< QString::fromUtf8( "攻略！英語リスニング" );
+		<< QString::fromUtf8( "エンジョイ・シンプル・イングリッシュ" )
+		<< QString::fromUtf8( "まいにちロシア語" ) << QString::fromUtf8( "ボキャブライダー" );
 QStringList CustomizeDialog::titleKeys = QStringList()
-		<< "basic1_title" << "basic2_title" << "basic3_title" << "timetrial_title"
-		<< "kaiwa_title" << "business1_title" << "business2_title" << "russian_title"
-		<< "yomu_title" << "enjoy_title"
+		<< "basic0_title" << "basic1_title" << "basic2_title" << "basic3_title" << "timetrial_title"
+		<< "kaiwa_title" << "business1_title" << "business2_title" << "gakusyu_title" << "gendai_title"
 		<< "chinese_title" << "french_title" << "italian_title" << "hangeul_title"
 		<< "german_title" << "spanish_title" << "levelup-chinese_title" << "levelup-hangeul_title"
-		<< "kouryaku_title";
+		<< "gendai_title" 
+		<< "enjoy_title" << "russian_title" << "vrradio_title";
 QStringList CustomizeDialog::fileNameKeys = QStringList()
-		<< "basic1_file_name" << "basic2_file_name" << "basic3_file_name" << "timetrial_file_name"
-		<< "kaiwa_file_name" << "business1_file_name" << "business2_file_name" << "russian_file_name"
-		<< "yomu_file_name" << "enjoy_file_name"
+		<< "basic0_file_name" << "basic1_file_name" << "basic2_file_name" << "basic3_file_name" << "timetrial_file_name"
+		<< "kaiwa_file_name" << "business1_file_name" << "business2_file_name" << "gakusyu_file_name" << "gendai_file_name" 
 		<< "chinese_file_name" << "french_file_name" << "italian_file_name" << "hangeul_file_name"
 		<< "german_file_name" << "spanish_file_name" << "levelup-chinese_file_name" << "levelup-hangeul_file_name"
-		<< "kouryaku_file_name";
+		<< "enjoy_file_name" << "russian_file_name" << "vrradio_file_name";
 
 void CustomizeDialog::formats( QString course, QString& titleFormat, QString& fileNameFormat ) {
 	int index = courses.indexOf( course );
@@ -88,12 +88,11 @@ CustomizeDialog::CustomizeDialog( Ui::DialogMode mode, QWidget *parent ) :
 
 void CustomizeDialog::settings( bool write ) {
 	QLineEdit* lineEdits[] = {
-		ui.lineEdit, ui.lineEdit_2, ui.lineEdit_3, ui.lineEdit_4,
+		ui.lineEdit_18, ui.lineEdit, ui.lineEdit_2, ui.lineEdit_3, ui.lineEdit_4,
 		ui.lineEdit_5, ui.lineEdit_6, ui.lineEdit_7, ui.lineEdit_8,
-		ui.lineEdit_17, ui.lineEdit_19,
-		ui.lineEdit_9, ui.lineEdit_10, ui.lineEdit_11, ui.lineEdit_12,
+		ui.lineEdit_17, ui.lineEdit_9, ui.lineEdit_10, ui.lineEdit_11, ui.lineEdit_12,
 		ui.lineEdit_13, ui.lineEdit_14, ui.lineEdit_15, ui.lineEdit_16,
-		ui.lineEdit_18,
+		ui.lineEdit_19, ui.lineEdit_20, ui.lineEdit_21,
 		NULL
 	};
 
