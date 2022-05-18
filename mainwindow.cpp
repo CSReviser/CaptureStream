9b4799439a2c6298f0e7134303ece20d339bb7d8
CSReviser <CSReviser@gmail.com>
2022-05-18 22:44:15 +0900

    拡張子mp3のビットレートを64kに変更

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -77,7 +77,7 @@ namespace {
 //			int day = regexp.cap( 2 ).toInt();
 //			result = QString( " (%1/%2/%3)" ).arg( regexp.cap( 3 ) )
 //					.arg( month, 2, 10, QLatin1Char( '0' ) ).arg( day, 2, 10, QLatin1Char( '0' ) );
-			result = QString( " (2022/05/15)" ); 
+			result = QString( " (2022/05/18)" ); 
 		}
 		return result;
 	}
